131280a298657779cbf16d898058f49c38e245d7
Erick <erick.cabral122@gmail.com>
2025-07-04 23:23:10 -0300

    feat: add CRUD routes for destinations

--- backend/src/routes/destinations.routes.ts
+++ backend/src/routes/destinations.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { destinationsController } from '../controllers/destinations.controller';
 import { authMiddleware } from '../middlewares/auth.middleware';
-import { validate, destinationSchema } from '../utils/validators';
+import { validate, destinationSchema, destinationUpdateSchema } from '../utils/validators';
 
 const destinationsRouter = Router();
 
@@ -9,5 +9,8 @@ destinationsRouter.use(authMiddleware);
 
 destinationsRouter.post('/', validate(destinationSchema), destinationsController.create);
 destinationsRouter.get('/', destinationsController.getAll);
+destinationsRouter.get('/:id', destinationsController.getById);
+destinationsRouter.put('/:id', validate(destinationUpdateSchema), destinationsController.update);
+destinationsRouter.delete('/:id', destinationsController.delete);
 
 export default destinationsRouter;
